8ebf7fcc6980c42fe9b9fbc7a9723c2762d3ffd6
warlockja <warlockja@gmail.com>
2025-03-14 18:10:48 +0300

    added source link to lottie card

--- src/app/cards/_components/CongratulationsCard/CongratulationsCard.tsx
+++ src/app/cards/_components/CongratulationsCard/CongratulationsCard.tsx
@@ -1,31 +1,34 @@
 "use client";
 import { Player } from "@lottiefiles/react-lottie-player";
 import "./congratulationscard.scss";
+import Link from "next/link";
 
 export default function CongratulationsCard() {
   return (
     <div className="congratulations_card">
-      <div className="ilustration">
-        <Player
-          src="https://assets10.lottiefiles.com/packages/lf20_LrcfNr.json"
-          background="white"
-          loop
-          autoplay
-        />
-        {/* <lottie-player
-          src="https://assets10.lottiefiles.com/packages/lf20_LrcfNr.json"
-          background="white"
-          speed="1"
-          loop
-          autoplay
-        ></lottie-player> */}
-      </div>
-      <h3>¡Congratulations!</h3>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipiscing elit ligula fusce urna
-        nisl, quam conubia nam sapien id penatibus.
-      </p>
-      <button>Continue</button>
+      <Link href={"https://codepen.io/designfenix/pen/yLJWMKV"}>
+        <div className="ilustration">
+          <Player
+            src="https://assets10.lottiefiles.com/packages/lf20_LrcfNr.json"
+            background="white"
+            loop
+            autoplay
+          />
+          {/* <lottie-player
+            src="https://assets10.lottiefiles.com/packages/lf20_LrcfNr.json"
+            background="white"
+            speed="1"
+            loop
+            autoplay
+          ></lottie-player> */}
+        </div>
+        <h3>¡Congratulations!</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipiscing elit ligula fusce
+          urna nisl, quam conubia nam sapien id penatibus.
+        </p>
+        <button>Continue</button>
+      </Link>
     </div>
   );
 }
