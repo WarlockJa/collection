e2d605842cae1ac3f6306e9943262b688e88318d
warlockja <warlockja@gmail.com>
2025-04-07 17:51:55 +0300

    @property does not convert to TW

--- src/app/cards/_components/InnerGlow/InnerGlow.tsx
+++ src/app/cards/_components/InnerGlow/InnerGlow.tsx
@@ -12,7 +12,6 @@ import Link from "next/link";
 
 export default function InnerGlow() {
   return (
-    // <div className="card">Magic and Shit</div>
     <div className="cardInnerGlow group hover:before:blur-[0.3em]">
       <Link href={"https://codepen.io/thebabydino/pen/WNVPdJg"} target="_blank">
         <Card className="aspect-video cursor-pointer border-slate-300 bg-transparent text-slate-300 transition-all group-hover:rounded-sm group-hover:ring-2">
